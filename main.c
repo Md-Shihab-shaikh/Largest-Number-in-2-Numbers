2ee1adbceb18a6e7c13791bed9f639416f0dedb3
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-06-11 09:57:14 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b;
+   scanf("%d %d",&a,&b);
+
+   if(a>b)
+   {
+       printf("a is large number\n",a);
+   }
+   else
+   {
+       printf("b is a large number \n",b);
+   }
+
+
+}
